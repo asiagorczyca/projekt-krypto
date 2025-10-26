defa497a09571ace0d51f30a0e710c33e4eaa9de
Turekkerem <keremozdogan@student.agh.edu.pl>
2025-10-26 16:19:26 +0100

    jakies jeszcze testy ale chyba jest spoko

--- w_pythonie/generowanie_duzej_ilosci_liczb_do_testu.py
+++ w_pythonie/generowanie_duzej_ilosci_liczb_do_testu.py
@@ -1,7 +1,7 @@
 import os
-k="python3 proba_generowania_liczb_losowych_bezpiecznie.py --bytes 1024"
+k="python3 proba_generowania_liczb_losowych_bezpiecznie.py --bytes 2048"
 with open("output.txt", "w", encoding="utf-8") as plik:
-    for i in range(100000):
+    for i in range(1000):
         #plik.write(f"\n### {k} ###\n")
         if (i+1)%20==0: print((i+1),".proba")
         wynik = os.popen(k).read()
