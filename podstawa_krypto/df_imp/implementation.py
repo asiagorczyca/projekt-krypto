ab69f867b95dffd877474a04f417e2bb0946a8f6
Joanna Gorczyca <jgorczyca@student.agh.edu.pl>
2025-12-06 21:51:17 +0100

    fixed that abomination of implementation - using gmpy2

--- podstawa_krypto/df_imp/implementation.py
+++ podstawa_krypto/df_imp/implementation.py
@@ -1,61 +1,53 @@
-import argparse
-import random
-#from podstawa_krypto.df_imp.generowanie_liczb_losowych import randomint,randomint_for_dh
+import gmpy2
+import sys
+import random 
 
-def is_probable_prime(n: int, rounds: int = 20) -> bool:
-    if n < 2:
-        return False
-    small_primes = (2,3,5,7,11,13,17,19,23,29)
-    for p in small_primes:
-        if n % p == 0:
-            return n == p
 
-    d = n - 1
-    s = 0
-    while d % 2 == 0:
-        d //= 2
-        s += 1
+sys.set_int_max_str_digits(10000)
 
-    for _ in range(rounds):
-        a = random.randint(2, n - 2)
-        x = pow(a, d, n)
-        if x == 1 or x == n - 1:
-            continue
-        for _ in range(s - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
-                break
-        else:
-            return False
-    return True
 
-def generate_random_odd(bits: int) -> int:
-    x = random.randint(2**(bits-1), 2**bits - 1)
-    if x % 2 == 0:
-        x += 1
-    return x
+def generate_prime(bits: int) -> int:
+    
+    seed = random.randint(0, 10**20)
+    rs = gmpy2.random_state(seed)
 
-def generate_prime(bits: int, rounds: int = 20) -> int:
-    while True:
-        p = generate_random_odd(bits)
-        if is_probable_prime(p, rounds=rounds):
-            return p
+    candidate = gmpy2.mpz_urandomb(rs, bits)
+    
+
+    candidate = gmpy2.bit_set(candidate, bits - 1)
+    candidate = gmpy2.bit_set(candidate, 0)
+    
+    p = gmpy2.next_prime(candidate)
+    return int(p)
 
 def find_generator(p: int) -> int:
-    for _ in range(1000):
-        g = random.randint(2, p - 2)
-        if pow(g, (p-1)//2, p) != 1:  
-            return g
-    return 2  
+    p_mpz = gmpy2.mpz(p)
+
+    if gmpy2.is_even(p_mpz): return 2
+
+    
+    rs = gmpy2.random_state()
+    p_minus_1_div_2 = (p_mpz - 1) // 2
+    
+    while True:
+        g = gmpy2.mpz_random(rs, p_mpz - 3) + 2
+        if gmpy2.powmod(g, 2, p_mpz) == 1: 
+            continue
+        if gmpy2.powmod(g, p_minus_1_div_2, p_mpz) == 1: 
+            continue
+            
+        return int(g)
 
 
 def generate_private(p: int) -> int:
-    return random.randint(2, p - 2)
-
-# change into one funkction 
+    rs = gmpy2.random_state()
+    priv = gmpy2.mpz_random(rs, gmpy2.mpz(p) - 3) + 2
+    return int(priv)
 
 def compute_public(g: int, priv: int, p: int) -> int:
-    return pow(g, priv, p)
+    res = gmpy2.powmod(gmpy2.mpz(g), gmpy2.mpz(priv), gmpy2.mpz(p))
+    return int(res)
 
 def compute_shared(priv: int, peer_pub: int, p: int) -> int:
-    return pow(peer_pub, priv, p)
+    res = gmpy2.powmod(gmpy2.mpz(peer_pub), gmpy2.mpz(priv), gmpy2.mpz(p))
+    return int(res)
